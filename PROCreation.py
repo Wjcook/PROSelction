2e1a8c3e3445f1e2ba1c4b59b567fad18b2cf46b
Will Cook <willnot3@gmail.com>
2022-02-14 22:10:16 -0800

    Implemented first version of solver: need to fix visualizations of the graph

--- PROCreation.py
+++ PROCreation.py
@@ -1,40 +1,96 @@
 from functools import cmp_to_key
-from random import randint
+import random
+
+import graphviz
+
+
+INIT_VALUE_RANGE = [0.1, 0.4]
+
 def comp(l, r):
+    if (l.set == r.set):
+        return 0
     if (len(l.set) < len(r.set)):
         return -1
     elif len(l.set) > len(r.set):
         return 1
     else:
-        return 0
+        l_iter = iter(l.set)
+        r_iter = iter(r.set)
+        while True:
+            next_l = next(l_iter)
+            next_r = next(r_iter)
+            if next_l < next_r:
+                return -1
+            elif next_l > next_r:
+                return 1
+
+    return 0
+
+def bisect_left(a, x, lo=0, hi=None, *, key=None):
+    """Return the index where to insert item x in list a, assuming a is sorted.
+    The return value i is such that all e in a[:i] have e < x, and all e in
+    a[i:] have e >= x.  So if x already appears in the list, a.insert(i, x) will
+    insert just before the leftmost x already there.
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+    """
+
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    # Note, the comparison uses "<" to match the
+    # __lt__() logic in list.sort() and in heapq.
+    if key is None:
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if a[mid] < x:
+                lo = mid + 1
+            else:
+                hi = mid
+    else:
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if key(a[mid], x) < 0:
+                lo = mid + 1
+            else:
+                hi = mid
+    return lo
 
 class PROSet:
-    def __init__(self, set, cost_to_go):
-        self.cost_to_go = cost_to_go
-        self.set = set
-        self.next = []
 
-    def get_next(pro_id):
-        return self.next[pro_id]
+    def __init__(self, set):
+        self.set = set
+        self.value = 0
+        self.next= []
+
+    def __str__(self):
+        return str(self.set) + ":" + "{0:.4f}".format(self.value)
 
 class PROSpace:
     def __init__(self, tot):
+        #store best 5-10 optimal sets for a given set size
+        self.tot = tot
         s = [i for i in range(tot)]
         self.nodes = []
         for i in range(1,1 << tot):
-            self.nodes.append(PROSet(set([s[j] for j in range(tot) if (i & (1 << j))]), randint(1,1000)))
+            self.nodes.append(PROSet(set([s[j] for j in range(tot) if (i & (1 << j))])))
         self.nodes.sort(key=cmp_to_key(comp))
+
 
         l = 0
         next_start = 0
         for i in range(len(self.nodes)):
             node = self.nodes[i]
+            if (l == 0 or l == 1):
+                node.value = random.uniform(INIT_VALUE_RANGE[0], INIT_VALUE_RANGE[1])
             if len(node.set) != l:
                 l += 1
                 if l != tot:
                     for j in range(i, len(self.nodes)):
                         if len(self.nodes[j].set) != l:
                             next_start = j
+                            break
             node.next = [None for j in range(tot)]
             for j in range(next_start, len(self.nodes)):
                 if len(self.nodes[j].set) == l+2:
@@ -42,14 +98,40 @@ class PROSpace:
                 diff = self.nodes[j].set - node.set
                 if len(diff) == 1:
                     for i in diff:
-                        node.next[i] = self.nodes[j]
+                        node.next[i] = j
+                        self.nodes[j].value = max(random.uniform(node.value, (l+1) / self.tot), self.nodes[j].value)
 
-        def get_root_nodes(self):
-            return self.nodes[:tot]
+    def visualize(self):
+        dot = graphviz.Digraph()
+        for i in range(len(self.nodes)):
+            dot.node(str(i), str(self.nodes[i]))
+            for j in range(len(self.nodes[i].next)):
+                if self.nodes[i].next[j] is not None:
+                    dot.edge(str(i), str(self.nodes[i].next[j]))
+
+        # create the starting node
+        dot.node("-1", "{}")
+        for i in range(self.tot):
+            dot.edge("-1", str(i))
+        dot.render('bet.gv', view=True)
+
+    def get_value(self, set):
+        i = bisect_left(self.nodes, set, key=comp)
+        if i < len(self.nodes):
+            return self.nodes[i].value
+        else:
+            raise ValueError
 
 
 
 
 
-if __name__ == "__main__":
-    space = PROSpace(20)
+
+
+
+# visualize tree exploration as well
+# dot(math viz)
+
+# if __name__ == "__main__":
+#     random.seed(10)
+#     space = PROSpace(5)
---
+++ solver.py
@@ -0,0 +1,247 @@
+from functools import cmp_to_key
+from random import randint
+from PyPDF2 import PdfFileMerger
+
+
+import numpy as np
+from PROCreation import *
+
+
+random.seed(10)
+# seed numpy random
+TOTAL_PROS = 5
+CANDIDATE_SPACE = PROSpace(TOTAL_PROS)
+GRAPHER = graphviz.Digraph()
+GRAPH_NUM = 0
+
+
+class PROSetState:
+    '''
+    Class that represents an actual set of PROs. This class stores information
+    about the actual orbits like what orbits are actually in the set, the orbits that
+    can be added to the set, and the information value/cost associated with the set.
+    '''
+
+    def __init__(self, set, total_PROs):
+        self.set = set
+        self.total_PROs = total_PROs
+        self.legal_PROs = []
+        for i in range(total_PROs):
+            if (i not in set):
+                self.legal_PROs.append(i)
+    def get_legal_pros(self):
+
+        return self.legal_PROs
+
+    def add_PRO(self, PRO):
+        new_set = self.set.copy()
+        new_set.add(PRO)
+        return PROSetState(new_set, self.total_PROs)
+
+    def get_value(self):
+        '''
+        Calls the PRO integrator to actually obtain the information value/cost of
+        the set of PROs.
+        '''
+        return CANDIDATE_SPACE.get_value(PROSet(self.set))
+
+
+
+class MCTSNode:
+    '''
+    Class that represents a node in the search tree of the Monte Carlo Solver.
+    Holds information about the node in the tree like how many times the node has
+    been visited, the sum of the information cost/value of all of its children nodes
+    and the nodes that have yet to be explored yet.
+    '''
+    def __init__(self, set_state, tot, target_size, parent=None, draw_tree=False):
+        self.PRO_state = set_state
+        # Init the available PROs to all legal pros then as we explore more children pop
+        # from this list
+        self.available_PROs = self.PRO_state.get_legal_pros()
+        self.children = []
+        self.num_visited = 0
+        self.sum_values = 0
+        self.parent = parent
+        self.tot = tot
+        self.target_size = target_size
+        self.draw = draw_tree
+
+
+    def is_terminal(self):
+        # Have we explored this node yet?
+        return len(self.PRO_state.set) == self.target_size
+
+    def is_expanded(self):
+        return len(self.available_PROs) == 0
+
+    def get_UCT(self, parent_sims, c):
+        # need to change UCT cost becuase it does not work for this purpose
+        if self.num_visited == 0:
+            return float('NaN')
+        return self.sum_values / self.num_visited + c * np.sqrt(np.log(parent_sims) / self.num_visited)
+
+    def greedy_choice(self):
+        children = [MCTSNode(self.PRO_state.add_PRO(choice), self.tot, \
+            self.target_size, parent=self) for choice in self.available_PROs]
+
+        vals = [child.PRO_state.get_value() for child in children]
+
+        return children[np.argmax(vals)]
+
+    def best_choice(self, c):
+        '''
+        Returns the child node that maximizes the Upper Confidence Bound for a certain
+        value of c.
+        '''
+        if len(self.children) == 0:
+            return self.greedy_choice()
+
+        UCTs = [child.get_UCT(self.num_visited, c) for child in self.children]
+        print(np.argmax(UCTs))
+        print(self.children[np.argmax(UCTs)].PRO_state.set)
+        return self.children[np.argmax(UCTs)]
+
+    def expand(self):
+        next_pro = self.available_PROs.pop()
+        next_child = MCTSNode(self.PRO_state.add_PRO(next_pro), self.tot, self.target_size, parent=self, draw_tree=True)
+        self.children.append(next_child)
+        return next_child
+
+    def select_expand(self,c):
+        cur = self
+
+        while not cur.is_terminal():
+
+            if not cur.is_expanded():
+
+                res = cur.expand()
+                return res
+            else:
+                # Continue down the treee
+                cur = cur.best_choice(c)
+        return cur
+
+    def sim(self, target_set_size):
+        global GRAPH_NUM
+
+        cur_pros = self.PRO_state
+        old_pros = self.PRO_state
+
+        while len(cur_pros.set) < target_set_size:
+            cur_pros = cur_pros.add_PRO(np.random.choice(cur_pros.get_legal_pros()))
+            if self.draw:
+                GRAPHER.node(str(cur_pros.set), "{0}".format(str(cur_pros.set)))
+                GRAPHER.edge(str(old_pros.set), str(cur_pros.set))
+                GRAPHER.render("graphs/graph{}.gv".format(GRAPH_NUM))
+                GRAPH_NUM += 1
+            old_pros = cur_pros
+        return cur_pros.get_value()
+
+    def back_prop(self, value):
+        self.num_visited += 1
+        self.sum_values += value
+        if self.parent is not None:
+            self.parent.back_prop(value)
+
+
+
+
+
+
+
+class MonteCarloSolver:
+    '''
+    Searches a large tree of nodes that represent sets of PROs for an optimal
+    subset of PROs that maximize/minimize some value.
+    '''
+
+    def __init__(self, c, iters, target_set_size, total_PROs, draw_tree=False):
+        '''
+        @PARAMS:
+        c - exploration parameter. The larger the value the more the solver will
+        prefer to explore new nodes rather than pursuing deeper into the tree
+
+        iters - how many iterations of the solving algorithm should run
+
+        target_set_size - The size of the subset of PROs to search for
+
+
+
+        '''
+
+        self.c=c
+        self.iters = iters
+        self.target_set_size = target_set_size
+        self.solved = False
+        '''
+        root - is the root node of the decision space we are trying to explore
+        This will be an object that will have functions to get the available actions
+        Make an action and return a cost
+        '''
+
+        self.root = MCTSNode(PROSetState(set([]), total_PROs), total_PROs, target_set_size, draw_tree=draw_tree)
+        self.draw = draw_tree
+
+
+
+
+    def add_node(self, state):
+        global GRAPH_NUM
+        GRAPHER.node(str(state.PRO_state.set), "{0}:{1}".format(str(state.PRO_state.set), \
+            state.get_UCT(state.parent.num_visited, self.c)))
+        if (state.parent is not None):
+            GRAPHER.edge(str(state.parent.PRO_state.set), str(state.PRO_state.set))
+        GRAPHER.render("graphs/graph{}.gv".format(GRAPH_NUM))
+        GRAPH_NUM += 1
+
+    def merge_pdf(self):
+
+        merger = PdfFileMerger()
+
+        for i in range(GRAPH_NUM):
+            merger.append("graphs/graph{}.gv.pdf".format(i))
+
+        merger.write("result.pdf")
+        merger.close()
+
+    def solve(self):
+        '''
+        The actual solving algorithm. The steps of the algorithm follow the main
+        steps of the MCTS algorithm: selection, expansion, simulation, and backprogation.
+        '''
+        for _ in range(self.iters):
+            state = self.root.select_expand(self.c)
+            if self.draw:
+                self.add_node(state)
+            # print(state.PRO_state.set)
+            value = state.sim(self.target_set_size)
+            state.back_prop(value)
+
+        self.solved = True
+
+        if self.draw:
+            self.merge_pdf()
+
+    def get_solution(self):
+        '''
+        Returns the optimal set of PROs that the solver has found so far.
+        '''
+        if not self.solved:
+            raise ValueError("'solve' has not been called")
+        cur_node = self.root
+        for _ in range(self.target_set_size):
+            cur_node = cur_node.best_choice(c=0.0)
+        return cur_node.PRO_state
+
+
+
+if __name__ == "__main__":
+
+    solver = MonteCarloSolver(np.sqrt(2), 10, 3, TOTAL_PROS, True)
+
+    solver.solve()
+    set = solver.get_solution().set
+    print(set)
+
+    # CANDIDATE_SPACE.visualize()
